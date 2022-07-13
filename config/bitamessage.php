227e0106365b37a73defc6a34903195cc2a73512
Milad Modaresi <miladmodaresi525@gmail.com>
2022-07-13 12:07:23 +0430

    feature: sms

--- config/bitamessage.php
+++ config/bitamessage.php
@@ -2,10 +2,11 @@
 
 use Bita\Message\Service\IpPanelService;
 use Bita\Message\Service\SmsIrService;
+use Bita\Message\Service\SmsIrV2Service;
 use Bita\Message\Service\TestService;
 
 return [
-    'driver' => 'ippanel', //default service is smsir 
+    'driver' => 'smsirv2', //default service is smsir
     'ipPanel' => [
         'name' => 'IpPanel',
         'endPoint' => env('BITA_MESSAGE_IP_PANEL_ENDPOINT'),
@@ -19,12 +20,19 @@ return [
         'apiKey' => env('BITA_MESSAGE_SMS_IR_API_KEY'),
         'secretKey' => env('BITA_MESSAGE_SMS_IR_SECRET_KEY')
     ],
+    'smsIrV2' => [
+        'name' => 'SmsIrV2',
+        'endPoint' => env('BITA_MESSAGE_SMS_IR_V2_ENDPOINT'),
+        'apiKey' => env('BITA_MESSAGE_SMS_IR_V2_API_KEY'),
+        'originator' => env('BITA_MESSAGE_SMS_IR_V2_ORIGINATOR')
+    ],
     'logs' => false,
     'tableName' => 'sms_logs', //default name is sms_logs
     //you can add driver here and set the key in driver
     'drivers' => [
         'ippanel' => IpPanelService::class,
         'smsir' => SmsIrService::class,
+        'smsirv2' => SmsIrV2Service::class,
         'test' => TestService::class
     ]
 ];
