4b36e542fb6c7251800b29b8757966aade9371be
Milad Modaresi <miladmodaresi525@gmail.com>
2022-08-11 15:22:32 +0430

    update: kavenegar

--- src/Service/KavenegarService.php
+++ src/Service/KavenegarService.php
@@ -23,6 +23,9 @@ class KavenegarService extends SmsBaseService implements SmsServiceInterface
     {
         $this->client = new Client([
             'base_uri' => $this->getEndPoint(),
+            'headers' => [
+                'Content-Type' => 'application/x-www-form-urlencoded',
+            ],
         ]);
     }
 
@@ -70,7 +73,7 @@ class KavenegarService extends SmsBaseService implements SmsServiceInterface
         }
         $key = $this->getToken();
         $body   = ['message' => $message, 'receptor' => $numbers, 'sender' => $this->getNumber()];
-        $result     = $this->client->post("$key/sms/send.json", ['body' => json_encode($body)]);
+        $result     = $this->client->post("$key/sms/send.json", ['form_params' => $body]);
 
         $res = json_decode($result->getBody(), true);
         $this->log($res, $body);
