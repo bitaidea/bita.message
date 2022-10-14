3a378b8a80693f129c392250a3ee949bb567c823
miladmodaresi <miladmodaresi525@gmail.com>
2022-10-14 19:42:31 +0330

    feature: document

--- src/Facades/Message.php
+++ src/Facades/Message.php
@@ -2,8 +2,14 @@
 
 namespace Bita\Message\Facades;
 
+use Bita\Message\Contract\Response\SendByPatternResponse;
+use Bita\Message\Contract\Response\SendResponse;
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method SendResponse send(string $message, array $numbers)
+ * @method SendByPatternResponse sendByPattern(string $template_id, string $number, array $parameters)
+ */
 class Message extends Facade
 {
     protected static function getFacadeAccessor()
