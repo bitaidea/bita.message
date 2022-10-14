b0f485fb4439170453e95344cc765cbbddefc786
miladmodaresi <miladmodaresi525@gmail.com>
2022-10-14 20:22:58 +0330

    chore: fix duc

--- src/Facades/Message.php
+++ src/Facades/Message.php
@@ -7,8 +7,8 @@ use Bita\Message\Contract\Response\SendResponse;
 use Illuminate\Support\Facades\Facade;
 
 /**
- * @method SendResponse send(string $message, array $numbers)
- * @method SendByPatternResponse sendByPattern(string $template_id, string $number, array $parameters)
+ * @method static SendResponse send(string $message, array $numbers)
+ * @method static SendByPatternResponse sendByPattern(string $template_id, string $number, array $parameters)
  */
 class Message extends Facade
 {
