a24eef07e15ba561523afdaf7c65e1fc4241afe5
milad modaresi <miladmodaresi525@gmail.com>
2023-01-25 16:40:01 +0330

    chore: add comment

--- src/Facades/Message.php
+++ src/Facades/Message.php
@@ -4,6 +4,13 @@ namespace Bita\Message\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * Class Message
+ * @package Bita\Message\Facades
+ *
+ * @method static string sendByPattern(string $template_id,int $number,array $parameters)
+ * @method static string send(string $message, array $numbers)
+ */
 class Message extends Facade
 {
     protected static function getFacadeAccessor()
