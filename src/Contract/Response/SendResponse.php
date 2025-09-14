8dacd1c62b8dde7786eed64f8aeced15bf448152
Davood <davoodha78@gmail.com>
2025-09-14 11:05:12 +0330

    add message ids to bulk send sms.ir

--- src/Contract/Response/SendResponse.php
+++ src/Contract/Response/SendResponse.php
@@ -6,8 +6,8 @@ use Bita\Message\Contract\BaseResponse;
 
 class SendResponse extends BaseResponse
 {
-    public function __construct(bool $status, $packId, string $message, $cost = 0)
+    public function __construct(bool $status, $packId, string $message, $cost = 0, $messageIds = [])
     {
-        $this->data = compact('status', 'packId', 'message', 'cost');
+        $this->data = compact('status', 'packId', 'message', 'cost', 'messageIds');
     }
 }
